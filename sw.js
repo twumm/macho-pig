886000fea55ac68395e48305900224a852e2a825
twumm <martint.mensah@gmail.com>
2018-07-03 12:36:43 +0000

    feat: Install sw, cache files for offline and intercept network request to display offline or online files.

---
+++ sw.js
@@ -0,0 +1,47 @@
+
+const dataCacheName = 'machoPigData-v1';
+const cacheName = 'machoPigCache-1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  // '/css/style.css',
+  // '/css/bootstrap.min.css',
+  '/js/app.js',
+  // '/js/lib/knockout-3.2.0.js'
+  // '/js/lib/jquery.js',
+  // '/js/lib/bootstrap.min.js',
+]
+
+// Install service worker and save assets/files to cache.
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  )
+});
+
+// Intercept requests and return cached version of assets, before checking network.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
